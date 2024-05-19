42c0a1c60975ce6bc5b56482cbbba994d7114b9b
dyn <you@example.com>
2024-05-19 23:18:42 +0900

    n commit

--- basecom/views.py
+++ basecom/views.py
@@ -1,5 +1,23 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render
+from django.core.paginator import Paginator
+
+
+from django.db import models
+from django.template.response import TemplateResponse
+import os
+from django.views.generic import TemplateView
+from django.shortcuts import redirect
+
+from django.core.exceptions import ObjectDoesNotExist
+
+
 
 def index(request):
-    return HttpResponse("aaaa")
+        context = dict()
+        context.update({
+            })
+
+        return render(request, "basecom/home.html", context)
+
