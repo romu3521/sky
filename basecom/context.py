4df164d23f868e06fabce61a2dcd765532bfc5e3
dyn <you@example.com>
2024-05-23 00:37:04 +0900

    n commit

--- basecom/context.py
+++ basecom/context.py
@@ -9,10 +9,16 @@ def get_context(request):
 
     path=f"{settings.BASE_DIR}/static/js/umd_main.js"
     is_file = os.path.isfile(path)
+    print(is_file)
+    print(path)
     if is_file:
         param = os.path.getmtime(path)
     else:
-        context['fileparam'] =param
+        pass
+
+
+    context['fileparam'] =param
+    print(context)
 
 
 
