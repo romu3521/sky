2bbed35399cd4bbb043622ad0467900e90d7df1a
dyn <you@example.com>
2024-06-01 02:37:43 +0900

    n commit

--- basecom/urls.py
+++ basecom/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView,ProfileView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="index"),
+    path("index.html", HomeView.as_view(), name="index"),
+    path("profile.html", ProfileView.as_view(), name="profile"),
 ]
--- basecom/views.py
+++ basecom/views.py
@@ -29,5 +29,19 @@ class HomeView(TemplateView):
         return render(request, self.template_name, context)
 
 
+class ProfileView(TemplateView):
+    model = None
+    template_name="basecom/profile.html"
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get(self, request, **kwargs):
+        context = dict()
+        context.update({
+            })
+
+        return render(request, self.template_name, context)
 
 
