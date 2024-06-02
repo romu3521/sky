af85f02b79bf6bec680a0bced7f4408db3e90af5
dyn <you@example.com>
2024-06-02 23:57:59 +0900

    n commit

--- basecom/models.py
+++ basecom/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+class FoPosts(models.Model):
+    id = models.AutoField(primary_key=True)
+    url_name = models.CharField(max_length=100, blank=True, null=True)
+    post_title = models.TextField(default="", blank=True, null=True)
+    post_content = models.TextField(default="", blank=True, null=True)
+    author_name = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now,blank=True, null=True)
+    updated_at = models.DateTimeField(default=timezone.now,blank=True, null=True)
+    access_count = models.PositiveBigIntegerField(default=0, blank=True, null=True)
+    def __str__(self):
+        return str(self.created_at)+"　　"+self.post_title
+    class Meta:
+        verbose_name_plural = "投稿"
