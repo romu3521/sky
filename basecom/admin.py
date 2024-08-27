850b925aebbc3adc9504d8275f2e730cf16a145a
romu3521 <you@example.com>
2024-08-28 04:13:15 +0900

    n commit

--- basecom/admin.py
+++ basecom/admin.py
@@ -6,6 +6,11 @@ from .models import FoContact
 from django.db.models.functions import Lower,Upper
 
 class FoContactAdmin(admin.ModelAdmin):
+    list_display=["contact_mail","short_text"]
+    def short_text(self, obj):
+        return obj.contact_content[0:80]+"..."
+
+    short_text.short_description = 'content'
     def get_ordering(self, request):
         return [Upper('created_at').desc()]
 
