c5463f0f52c19b4efa0a7168ccdb01c9a548c92d
dyn <chro.3521.dyn@gmail.com>
2025-05-13 03:36:07 +0900

    n commit

--- demosite/urls.py
+++ demosite/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import HomeView,Demo1View
 
 urlpatterns = [
-    path("", HomeView.as_view(), name="index"),
+   # path("", HomeView.as_view(), name="index"),
     path("demo1", Demo1View.as_view(), name="demo1"),
 ]
